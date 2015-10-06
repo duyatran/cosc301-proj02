1cbceed9069102100acbca2604b6e0e8b44e6fa2
Duy Tran <dtran@colgate.edu>
2015-10-05 20:57:58 -0400

    Stage 1 - parameters checking and mode command left

--- main.c
+++ main.c
@@ -20,25 +20,33 @@ void check_input(char **str) {
 	}
 }
 
-char** tokenify(const char *s) {
+void print_tokens(char *tokens[]) {
+    int i = 0;
+    while (tokens[i] != NULL) {
+        printf("Token %d: %s\n", i+1, tokens[i]);
+        i++;
+    }
+}
+
+char** tokenify(const char *s, const char *delimit) {
     char *copy1 = strdup(s);
     char *saveptr1;
-	char *count = strtok_r(copy1, " \n\t", &saveptr1);
+	char *count = strtok_r(copy1, delimit, &saveptr1);
     int len = 0;
     while (count != NULL) {
 		len++;
-		count = strtok_r(NULL, " \n\t", &saveptr1);
+		count = strtok_r(NULL, delimit, &saveptr1);
 	}
 	free(copy1);
 	
 	char **tokens = malloc(sizeof(char *) * (len+1));
     char *copy2 = strdup(s);
     char *saveptr2;
-    char *token = strtok_r(copy2, " \n\t", &saveptr2);
+    char *token = strtok_r(copy2, delimit, &saveptr2);
     int i = 0;
     for (i = 0; token != NULL; i++) {
 		tokens[i] = strdup(token);
-		token = strtok_r(NULL, " \n\t", &saveptr2);
+		token = strtok_r(NULL, delimit, &saveptr2);
 	}
 	free(copy2);
 	tokens[i] = NULL;
@@ -48,63 +56,73 @@ char** tokenify(const char *s) {
 void free_tokens(char **tokens) {
     int i = 0;
     while (tokens[i] != NULL) {
-        free(tokens[i]); // free each string
+        free(tokens[i]); // free each token
         i++;
     }
-    free(tokens); // then free the array
+    free(tokens); // then free the array of tokens
 }
 
-void create_process(char ***proc) {
-    pid_t tpid;
-	int child_pid = fork();
-	int child_status;
-    if (child_pid < 0) {
-        // fork failed;
-        fprintf(stderr, "fork failed\n");
-        //exit(1);
-    } 
-    
-    else if (child_pid == 0) {
-	// child: execv this command line
-		if (execv(*proc[0],*proc) < 0) {
-			fprintf(stderr, "execv failed: %s\n", strerror(errno));
-		}
-    } 
-    
-    else {
-        // parent waits until child terminated
-		do {
-			tpid = wait(&child_status);
-			if (tpid == child_pid) {
-				//do something, probably break
-				break;
+void create_process(char **commands, int *exit_code) {
+	int mode = 0; // 0 is sequential, 1 is parallel
+	int special= 0;
+	char **one_command = tokenify(*commands, " \n\t");
+	if ((strcmp(one_command[0], "exit")) == 0) {
+		special = 1;
+		*exit_code = 1;
+	}
+	//else if (strcmp(one_command[0], "mode") {
+		
+	//}
+	if (special == 0) {
+		pid_t tpid;
+		int child_pid = fork();
+		int child_status;
+		if (child_pid < 0) {
+			// fork failed;
+			fprintf(stderr, "fork failed\n");
+			//exit(1);
+		} 
+		else if (child_pid == 0) {
+		// child: execv this command line
+			if (execv(one_command[0],one_command) < 0) {
+				fprintf(stderr, "execv failed: %s\n", strerror(errno));
 			}
-     } while(tpid != child_pid);
-    }
-}
-
-void print_tokens(char *tokens[]) {
-    int i = 0;
-    while (tokens[i] != NULL) {
-        printf("Token %d: %s\n", i+1, tokens[i]);
-        i++;
-    }
+		} 
+		else {
+			// parent waits until child terminated
+			do {
+				tpid = wait(&child_status);
+				if (tpid == child_pid) { // if child terminated
+					free_tokens(one_command);
+					break;
+				}
+			} while(tpid != child_pid);
+		}
+	}
 }
 
 void start_prompt() {
 	char buffer[1024];
-	char spaces[] = " \n\t";
-	char end_command = ";";
+	int ex = 0;
 	while (fgets(buffer, 1024, stdin) != NULL) {
 		check_input(&buffer);
-		
-		char **args = tokenify(buffer);
-		create_process(&args);
-		free_tokens(args);
-		
+		char **commands = tokenify(buffer, ";");
+		printf("list of commands\n");
+		print_tokens(commands);
+		printf("end\n");
+		int i = 0;
+		while (commands[i] != NULL) {
+			create_process(&(commands[i]), &ex);
+			i++;
+		}
+		free_tokens(commands);
+		if (ex == 1) {
+			exit(1);
+		}
 		printf("%s", PROMPT);
 		fflush(stdout);
 	}
+	printf("\n"); // EOF reached, just print new line before exiting
 }
 
 int main(int argc, char **argv) {
